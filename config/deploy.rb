a3e641db29df998189ac6c69b517732ab70a972c
tetuyoko <tyokoyama53@gmail.com>
2015-01-27 19:37:23 +0900

    rbenv 設定アップデート

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,8 @@ set :application, 'team_dashboard'
 set :repo_url, 'git@github.com:tetuyoko/team_dashboard.git'
 set :keep_releases, 5
 
-set :rbenv_type, :user # :system or :user
+set :rbenv_path, '/home/azureuser/.rbenv'
+set :rbenv_type, :system # :system or :user
 set :rbenv_ruby, '2.0.0-p247'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
